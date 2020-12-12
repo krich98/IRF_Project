e2de8109766d4df9610feced10d2224bc24b6b09
Ábel Richárd Kádár <abel.kadar@stud.uni-corvinus.hu>
2020-12-12 11:33:47 +0100

    custombutton class

---
+++ Foodworks/Foodworks/Entities/CustomButton.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Foodworks.Entities
+{
+    class CustomButton : Button
+    {
+        public CustomButton(Color color)
+        {
+            FlatStyle = FlatStyle.Flat;
+            FlatAppearance.BorderSize = 0;
+            Font = new Font("Microsoft Sans Serif", 25);
+            BackColor = color;
+        }
+        
+    }
+}
--- Foodworks/Foodworks/Form1.cs
+++ Foodworks/Foodworks/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Foodworks.Entities;
 
 namespace Foodworks
 {
@@ -15,6 +16,10 @@ namespace Foodworks
         public Form1()
         {
             InitializeComponent();
+
         }
+       public void Buttonok()
+       { 
+       }
     }
 }

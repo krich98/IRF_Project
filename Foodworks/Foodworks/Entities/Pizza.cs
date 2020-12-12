dc0e406fa26c721cb1d256a705e29f0e4b0e0b8c
Ábel Richárd Kádár <abel.kadar@stud.uni-corvinus.hu>
2020-12-12 13:08:43 +0100

    GetPizzas

---
+++ Foodworks/Foodworks/Entities/Pizza.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Foodworks.Entities
+{
+    class Pizza
+    {
+        public int Sorszam { get; set; }
+        public string Nev { get; set; }
+        public int Ar { get; set; }
+        public string Leiras { get; set; }
+    }
+}
--- Foodworks/Foodworks/UserControls/UcPizza.cs
+++ Foodworks/Foodworks/UserControls/UcPizza.cs
@@ -3,18 +3,42 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Foodworks.Entities;
 
 namespace Foodworks.UserControls
 {
     public partial class UcPizza : UserControl
     {
+        List<Pizza> pizzak = new List<Pizza>();
         public UcPizza()
         {
             InitializeComponent();
+            GetPizzas();
+        }
+        public void GetPizzas()
+        {
+            string path = "Files/Pizzak.cs";
+            
+            using (StreamReader sr = new StreamReader(path, Encoding.UTF8))
+            {
+                while (!sr.EndOfStream)
+                {
+                    var line = sr.ReadLine().Split(';');
+                    Pizza p = new Pizza();
+                    p.Sorszam = int.Parse(line[0]);
+                    p.Nev = line[1];
+                    p.Ar = int.Parse(line[2]);
+                    p.Leiras = line[3];
+                    pizzak.Add(p);
+
+                }
+            }
+            
         }
     }
 }
